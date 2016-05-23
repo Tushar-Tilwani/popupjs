03f387d4e869c0e0af393c97deb80de88fb85cf5
tushar <tushar.tilwani@live.in>
2016-05-23 00:02:55 -0400

    added closure

--- script.js
+++ script.js
@@ -1,118 +1,120 @@
  /*jshint esversion: 6 */
-var Popups = function(optionsDefaults) {
-"use strict";
- 
+ var Popups = (function() {
+     "use strict";
+
+
+     var defaults = {
+         className: "popup",
+         styles: {
+             position: "absolute",
+             height: 200,
+             width: 200,
+             backgroundColor: "#b3d9ff",
+             top: 100,
+             left: 100,
+             overflowY: "scroll",
+             padding: 10,
+             display: "none",
+         },
+         header: "The Awesome Popup!",
+         content: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo."
+     };
+
      /**
-     * Simple is object check.
-     * @param item
-     * @returns {boolean}
-     */
-    function isObject(item) {
-        return (item && typeof item === 'object' && !Array.isArray(item) && item !== null);
-    }
+      * Simple is object check.
+      * @param item
+      * @returns {boolean}
+      */
+     function isObject(item) {
+         return (item && typeof item === 'object' && !Array.isArray(item) && item !== null);
+     }
 
-    /**
-     * Deep merge two objects.
-     * @param target
-     * @param source
-     */
-    function mergeDeep(target, source) {
-        if (isObject(target) && isObject(source)) {
-            Object.keys(source).forEach(key => {
-                if (isObject(source[key])) {
-                    if (!target[key]) Object.assign(target, {
-                        [key]: {}
-                    });
-                    mergeDeep(target[key], source[key]);
-                } else {
-                    Object.assign(target, {
-                        [key]: source[key]
-                    });
-                }
-            });
-        }
-        return target;
-    }
-    var popups = {};
+     /**
+      * Deep merge two objects.
+      * @param target
+      * @param source
+      */
+     function mergeDeep(target, source) {
+         if (isObject(target) && isObject(source)) {
+             Object.keys(source).forEach(key => {
+                 if (isObject(source[key])) {
+                     if (!target[key]) Object.assign(target, {
+                         [key]: {}
+                     });
+                     mergeDeep(target[key], source[key]);
+                 } else {
+                     Object.assign(target, {
+                         [key]: source[key]
+                     });
+                 }
+             });
+         }
+         return target;
+     }
 
-    var defaults = {
-        className: "popup",
-        styles: {
-            position: "absolute",
-            height: 200,
-            width: 200,
-            backgroundColor: "#b3d9ff",
-            top: 100,
-            left: 100,
-            overflowY: "scroll",
-            padding: 10,
-            display: "none",
-        },
-        header: "The Awesome Popup!",
-        content: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo."
-    };
-    
-    optionsDefaults = mergeDeep(defaults,optionsDefaults);
+     function getTemplate(id, options) {
 
-    function getTemplate(id, options) {
-        options = mergeDeep(defaults,options);
+         var div = document.createElement('div');
+         div.className = options.className;
+         div.id = id;
+         
+         var styles = options.styles;
 
-        var div = document.createElement('div');
-        div.className = options.className;
-        div.id = id;
-        
-        var styles = optionsDefaults.styles;
-        
-        for (var i in styles) {
-            if (typeof styles[i] === "number") {
-                styles[i] = styles[i] + "px";
-            }
-            div.style[i] = styles[i];
-        }
+         Object.keys(styles).forEach(key => {
+             if (typeof styles[key] === "number") {
+                 styles[key] = styles[key] + "px";
+             }
+             div.style[key] = styles[key];
+         });
 
-        var innerHTMLStr = "<h3 class='head'>" + options.header + "</h3>" +
-            "<div class='content'>" + options.content + "</div>";
-        div.innerHTML = innerHTMLStr;
+         var innerHTMLStr = "<h3 class='head'>" + options.header + "</h3>" +
+             "<div class='content'>" + options.content + "</div>";
+         div.innerHTML = innerHTMLStr;
 
-        return div;
-    }
+         return div;
+     }
 
-    function removeTemplate(id) {
-        var ele = popups[id];
-        ele.parentNode.removeChild(ele);
-        delete popups[id];
-    }
+     function removeTemplate(ele) {
+         ele.parentNode.removeChild(ele);
+         delete popups[id];
+     }
 
-    function checkPopup(id){
-        var popup = popups[id];
-        if (!popup) {
-            throw new Error("Popup not found");
-        }
-        return popup;
-    }
-    
-    this.add = function(id, options) {
-        document.body.appendChild(getTemplate(id, options));
-        if (popups[id]) {
-            removeTemplate(id);
-            console.warn("Popup was overriden!");
-        }
-        popups[id] = document.getElementById(id);
-    };
+     function checkPopup(popups, id) {
+         var popup = popups[id];
+         if (!popup && throwErr) {
+             throw new Error("Popup not found");
+         }
+         return popup;
+     }
 
-    this.show = function(id) {
-        var popup = checkPopup(id);
-        popup.style.display = "block";
-    };
+     return function(optionsDefaults) {
+         var popups = {};
+         optionsDefaults = mergeDeep(mergeDeep({}, defaults), optionsDefaults);
 
-    this.hide = function(id) {
-        var popup = checkPopup(id);
-        popup.style.display = "none";
-    };
+         this.add = function(id, options) {
+             options = mergeDeep(mergeDeep({}, optionsDefaults), options);
+             document.body.appendChild(getTemplate(id, options));
+             var popup = popups[id];
+             if (popup) {
+                 removeTemplate(popup);
+                 console.warn("Popup was overriden!");
+             }
+             popups[id] = document.getElementById(id);
+         };
 
-    this.destroy = function(id) {
-        checkPopup(id);
-        removeTemplate(id);
-    };
+         this.show = function(id) {
+             var popup = checkPopup(popups, id);
+             popup.style.display = "block";
+         };
 
-};
+         this.hide = function(id) {
+             var popup = checkPopup(popups, id);
+             popup.style.display = "none";
+         };
+
+         this.destroy = function(id) {
+             removeTemplate(checkPopup(popups, id));
+         };
+
+     };
+ })()
